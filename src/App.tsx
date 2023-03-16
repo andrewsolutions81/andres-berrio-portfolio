ade0ed7e0681474d59c201d1b3ed0f88b8f5b853
andrew-berrio <andrewsolutions81@gmail.com>
2023-03-16 14:55:31 -0500

    create about contact and projects components

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import Header from './Components/Header/Header';
+import Home from "./Pages/Home/Home"
 import './App.css';
 
 function App() {
+  //tabs logic goes here
   return (
     <main className="App">
       <Header/>
+      <Home />
     </main>
   );
 }
--- src/Components/About/About.tsx
+++ src/Components/About/About.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
+import "./About.css"
 
 export default function About() {
   return (
     <div className="About">
-      <h1 className="about">About</h1>
-      <h2 className="name">Andres Berrio</h2>
+      <h1 className="about-name">Andres Berrio</h1>
       <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ratione repellendus odio magni dolores quasi, doloribus rerum culpa atque necessitatibus, ut aliquam et id molestiae fuga. Laudantium a velit nulla officiis!</p>
     </div>
   )
