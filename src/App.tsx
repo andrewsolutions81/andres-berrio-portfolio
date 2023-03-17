4ec05f467f705e32d258e521948c185b887d4532
andrew-berrio <andrewsolutions81@gmail.com>
2023-03-17 16:18:09 -0500

    create router

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,24 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import Header from './Components/Header/Header';
 import Home from "./Pages/Home/Home"
+import About from "./Components/About/About"
+import Projects from './Components/Projects/Projects'
+import Contact from './Components/Contact/Contact'
 import './App.css';
 
 function App() {
-  //tabs logic goes here
   return (
-    <main className="App">
+    <div className="App">
       <Header/>
-      <Home />
-    </main>
+      <Routes>
+        <Route path="*" element={<Home />} />
+        <Route path='/' element={<Home/>} />
+        <Route path='/about' element={<About/>} />
+        <Route path='/projects' element={<Projects/>} />
+        <Route path='/contact' element={<Contact/>} />
+      </Routes>
+    </div>
   );
 }
 
