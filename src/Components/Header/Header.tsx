9cd897ca3b1e2c574e25f3f06befa23b853f36fb
Andres Berrio (Andrew) <andrewsolutions81@gmail.com>
2023-03-23 18:44:52 -0500

    work on about styles create types for data

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,44 +1,68 @@
 import React from "react";
-import { useNavigate } from "react-router-dom"
-import Mail from '../../img/mail-icon.png'
-import Git from '../../img/github-icon.png'
-import Insta from "../../img/instagram-icon.png"
+import { useNavigate } from "react-router-dom";
+import Mail from "../../img/mail-icon.png";
+import Git from "../../img/github-icon.png";
+import Insta from "../../img/instagram-icon.png";
 import "./Header.css";
 
 export default function Header() {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
-  const navigateHome = () => navigate("/home")
-  const navigateAbout = () => navigate("/about")
-  const navigateProjects = () => navigate("/projects")
-  const navigateContact = () => navigate("/contact")
-
+  const navigateHome = () => navigate("/home");
+  const navigateAbout = () => navigate("/about");
+  const navigateProjects = () => navigate("/projects");
+  const navigateContact = () => navigate("/contact");
 
   return (
     <div className="Header">
-      <main className="header-titles" >
-        <h1 className="header-h1" onClick={navigateHome} >Andrew Solutions</h1>
-        <h2 className="header-h2" onClick={navigateHome} >Developer</h2>
+      <main className="header-titles">
+        <h1 className="header-h1" onClick={navigateHome}>
+          Andrew Solutions
+        </h1>
+        <h2 className="header-h2" onClick={navigateHome}>
+          Developer
+        </h2>
       </main>
       <section className="tabs">
-        <span className="about-tab" onClick={navigateAbout}>About</span>
-        <span className="projects-tab" onClick={navigateProjects}>Projects</span>
-        <span className="contact-tab" onClick={navigateContact}>Contact</span>
+        <span className="about-tab" onClick={navigateAbout}>
+          About
+        </span>
+        <span className="projects-tab" onClick={navigateProjects}>
+          Projects
+        </span>
+        <span className="contact-tab" onClick={navigateContact}>
+          Contact
+        </span>
       </section>
       <section className="social-media">
         <div className="header-mail social-media-item">
-          <img src={Mail} alt="mail icon" loading="lazy" className="header-img__mail" />
+          <img
+            src={Mail}
+            alt="mail icon"
+            loading="lazy"
+            className="header-img__mail"
+          />
         </div>
         <div className="header-git social-media-item">
-          <img src={Git} alt="git icon" loading="lazy" className="header-img__git" />
+          <img
+            src={Git}
+            alt="git icon"
+            loading="lazy"
+            className="header-img__git"
+          />
         </div>
         <div className="header-insta social-media-item">
-          <img src={Insta} alt="instagram icon" loading="lazy" className="header-img__insta" />
+          <img
+            src={Insta}
+            alt="instagram icon"
+            loading="lazy"
+            className="header-img__insta"
+          />
         </div>
       </section>
       <div className="theme">
-        <button className="theme-btn">Theme</button>
-        <span className="theme-text">dark / light</span>
+        <button className="theme-btn">Language</button>
+        <span className="theme-text">English / Espanish</span>
       </div>
     </div>
   );
---
+++ src/types/types.ts
@@ -0,0 +1,13 @@
+export type projectType = {
+  id: string;
+  link: string;
+  title: string;
+  img: string;
+  desciption: string;
+  role: string;
+  startDate: string;
+  endDate: string;
+  tech: string[];
+};
+
+export type projectsType = projectType[];
