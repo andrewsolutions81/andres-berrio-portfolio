9c8e0d940dbfd8296f4d4a18d462691cc89e4ab1
Andres Berrio (Andrew) <andrewsolutions81@gmail.com>
2023-03-27 10:53:29 -0500

    fix fake fetch. (#7) (#8)

--- src/data/projectsData.ts
+++ src/data/projectsData.ts
@@ -1,88 +1,88 @@
 import { projectsType, projectType } from "../types/types";
 
-export const projects: projectsType = [
-  {
-    id: "id1",
-    link: "hey I am a link",
-    title: "Advance",
-    img: "i shoud bring an image",
-    desciption: "social media",
-    role: "full stack developer",
-    startDate: "2022 / 12 / 02",
-    endDate: "2022 / 12 / 02",
-    tech: ["javaScript", "nodeJs", "Express", "mongoDb", "mongoose", "React"],
-  },
-  {
-    id: "id2",
-    link: "hey I am a link",
-    title: "Advance",
-    img: "i shoud bring an image",
-    desciption: "social media",
-    role: "full stack developer",
-    startDate: "2022 / 12 / 02",
-    endDate: "2022 / 12 / 02",
-    tech: ["javaScript", "nodeJs", "Express", "mongoDb", "mongoose", "React"],
-  },
-  {
-    id: "id3",
-    link: "hey I am a link",
-    title: "Advance",
-    img: "i shoud bring an image",
-    desciption: "social media",
-    role: "full stack developer",
-    startDate: "2022 / 12 / 02",
-    endDate: "2022 / 12 / 02",
-    tech: ["javaScript", "nodeJs", "Express", "mongoDb", "mongoose", "React"],
-  },
-];
+// export const projects: projectsType = [
+//   {
+//     id: "id1",
+//     link: "hey I am a link",
+//     title: "Advance",
+//     img: "i shoud bring an image",
+//     desciption: "social media",
+//     role: "full stack developer",
+//     startDate: "2022 / 12 / 02",
+//     endDate: "2022 / 12 / 02",
+//     tech: ["javaScript", "nodeJs", "Express", "mongoDb", "mongoose", "React"],
+//   },
+//   {
+//     id: "id2",
+//     link: "hey I am a link",
+//     title: "Advance",
+//     img: "i shoud bring an image",
+//     desciption: "social media",
+//     role: "full stack developer",
+//     startDate: "2022 / 12 / 02",
+//     endDate: "2022 / 12 / 02",
+//     tech: ["javaScript", "nodeJs", "Express", "mongoDb", "mongoose", "React"],
+//   },
+//   {
+//     id: "id3",
+//     link: "hey I am a link",
+//     title: "Advance",
+//     img: "i shoud bring an image",
+//     desciption: "social media",
+//     role: "full stack developer",
+//     startDate: "2022 / 12 / 02",
+//     endDate: "2022 / 12 / 02",
+//     tech: ["javaScript", "nodeJs", "Express", "mongoDb", "mongoose", "React"],
+//   },
+// ];
 
-// function fakeFetch(): Promise<projectsType[]> {
-//   return new Promise((resolve) => {
-//     setTimeout(() => {
-//       const projects:<any> projectsType[] = [
-//         {
-//           id: "1",
-//           link: "hey I am a link",
-//           title: "Advance",
-//           img: "i shoud bring an image",
-//           desciption: "social media",
-//           role: "full stack developer",
-//           startDate: "2022 / 12 / 02",
-//           endDate: "2022 / 12 / 02",
-//           tech: ["javaScript", "nodeJs", "Express", "mongoDb", "mongoose", "React"],
-//         },
-//         {
-//           id: "id2",
-//           link: "hey I am a link",
-//           title: "Advance",
-//           img: "i shoud bring an image",
-//           desciption: "social media",
-//           role: "full stack developer",
-//           startDate: "2022 / 12 / 02",
-//           endDate: "2022 / 12 / 02",
-//           tech: ["javaScript", "nodeJs", "Express", "mongoDb", "mongoose", "React"],
-//         },
-//         {
-//           id: "id3",
-//           link: "hey I am a link",
-//           title: "Advance",
-//           img: "i shoud bring an image",
-//           desciption: "social media",
-//           role: "full stack developer",
-//           startDate: "2022 / 12 / 02",
-//           endDate: "2022 / 12 / 02",
-//           tech: ["javaScript", "nodeJs", "Express", "mongoDb", "mongoose", "React"],
-//         },
-//       ];
-//       resolve(projects);
-//     }, 1000); // 1 second delay
-//   });
-// }
+function fakeFetch(): Promise<projectsType[]> {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      const projects: projectsType[] = [
+        [
+          {
+            id: "id1",
+            link: "hey I am a link",
+            title: "Advance",
+            img: "i shoud bring an image",
+            desciption: "social media",
+            role: "full stack developer",
+            startDate: "2022 / 12 / 02",
+            endDate: "2022 / 12 / 02",
+            tech: ["javaScript", "nodeJs", "Express", "mongoDb", "mongoose", "React"],
+          },
+          {
+            id: "id2",
+            link: "hey I am a link",
+            title: "Advance",
+            img: "i shoud bring an image",
+            desciption: "social media",
+            role: "full stack developer",
+            startDate: "2022 / 12 / 02",
+            endDate: "2022 / 12 / 02",
+            tech: ["javaScript", "nodeJs", "Express", "mongoDb", "mongoose", "React"],
+          },
+          {
+            id: "id3",
+            link: "hey I am a link",
+            title: "Advance",
+            img: "i shoud bring an image",
+            desciption: "social media",
+            role: "full stack developer",
+            startDate: "2022 / 12 / 02",
+            endDate: "2022 / 12 / 02",
+            tech: ["javaScript", "nodeJs", "Express", "mongoDb", "mongoose", "React"],
+          },
+        ]
+      ];
+      resolve(projects);
+    }, 1000); // 1 second delay
+  });
+}
 
-// export const fetchProjects = async ()=> {
-//   const projects = await fakeFetch();
-//   console.log(projects)
-//   return projects
-// }
-
-
+export const fetchProjects = async ()=> {
+  const projects = await fakeFetch();
+  console.log(projects)
+  return projects
+}
