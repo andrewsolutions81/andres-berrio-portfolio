c0b6aaf61d99b0154402b36364daa3e060002100
Andres Berrio (Andrew) <andrewsolutions81@gmail.com>
2023-04-04 17:22:02 -0500

    Develop (#10)
    * fix fake fetch. (#7)
    
    * Projects 1 (#9)
    
    * fix fake fetch. (#7) (#8)
    
    * feat project page mvp

--- src/Components/Projects/Projects.tsx
+++ src/Components/Projects/Projects.tsx
@@ -1,20 +1,71 @@
-import React, {useState, useEffect}from 'react'
-// import {fetchProjects} from '../../data/projectsData'
-import { projects } from '../../data/projectsData';
-import "./Projects.css"
+import React from "react";
+import { projectsType, projectType } from "../../types/types";
+import advanceimg from "../../img/advanceimg.png";
+import tuvideoimg from "../../img/tuvideoimg.png";
+import udemyimg from "../../img/udemyimg.png";
+import "./Projects.css";
+
+export const projects: projectsType = [
+  {
+    id: "3",
+    link: "https://advance-liart.vercel.app/home",
+    title: "Advance",
+    img: advanceimg,
+    description: "social media platform for uploading photos and videos.",
+    role: "full stack developer",
+    startDate: "2022 / 12 / 02",
+    endDate: "2022 / 12 / 02",
+    tech: ["javaScript", "nodeJs", "Express", "mongoDb", "mongoose", "React"],
+  },
+  {
+    id: "2",
+    link: "https://mir-topv24-udemy-frontend.vercel.app/",
+    title: "Udemy(clone)",
+    img: udemyimg,
+    description: "social media",
+    role: "full stack developer , Back end Architect",
+    startDate: "2022 / 12 / 02",
+    endDate: "2022 / 12 / 02",
+    tech: ["javaScript", "nodeJs", "Express", "mongoDb", "mongoose", "React"],
+  },
+  {
+    id: "1",
+    link: "https://tuvideo-frontend-ten.vercel.app/",
+    title: "TuVideo",
+    img: tuvideoimg,
+    description:
+      "Video platform for uploading video, following channels, comment and like videos!",
+    role: "Full Stack Developer",
+    startDate: "2022 / 12 / 02",
+    endDate: "2022 / 12 / 02",
+    tech: ["javaScript", "nodeJs", "Express", "mongoDb", "mongoose", "React"],
+  },
+];
 
 export default function Projects() {
-  const data = projects
-
   return (
     <div className="Projects">
-      <main className="project-main">
-        <div className="test">test
-        <div className="test-data">
-          {data[0].title}
-        </div>
-        </div>
-      </main>
+      <ul className="projects-ul">
+        {projects.map((project: projectType) => (
+          <li className="projects-li" key={project.id}>
+            <main className="project-container">
+              <section className="project-info">
+                <h2 className="porject-title">{project.title}</h2>
+                <span className="project-role">{project.role}</span>
+                <p className="project-description">{project.description}</p>
+                <ul className="skills-container">
+                  {project.tech.map((t: string) => (
+                    <li className="skills" key={t}>{`| ${t} `}</li>
+                  ))}
+                </ul>
+              </section>
+              <section className="project-image">
+                <img className="img" src={project.img} alt={project.title} loading="lazy" />
+              </section>
+            </main>
+          </li>
+        ))}
+      </ul>
     </div>
-  )
+  );
 }
