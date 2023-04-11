ed7ba0db56bdf21997242e5a6a1e85375872feea
Andres Berrio (Andrew) <andrewsolutions81@gmail.com>
2023-04-11 11:20:14 -0500

    Contact 2 (#12)
    * fix fake fetch. (#7)
    
    * Projects 1 (#9)
    
    * fix fake fetch. (#7) (#8)
    
    * feat project page mvp
    
    * feature: contact page event change working

--- src/Components/Contact/Contact.tsx
+++ src/Components/Contact/Contact.tsx
@@ -1,11 +1,73 @@
-import React from 'react'
-import "./Contact.css"
+import React, { useState } from "react";
+import { FormDataType } from "../../types/types";
+import "./Contact.css";
 
-export default function Contact() {
+
+
+const Contact: React.FC = () => {
+  const [formData, setFormData] = useState<FormDataType>({
+    name: "",
+    email: "",
+    message: "",
+  });
+
+  const handleInputChange = (
+    event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    const { name, value } = event.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log(formData);
+  };
+
   return (
     <div className="Contact">
-      <h1 className="contact">Contact me</h1>
-      <p className="contact-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro culpa voluptas, minus vero ullam doloribus molestias voluptatibus eligendi deserunt nisi sint, distinctio rem delectus deleniti explicabo ipsum iusto. Veritatis, cupiditate?</p>
+      <h1 className="contact-h1">Contact me!</h1>
+      <form className="contact-form" onSubmit={handleSubmit}>
+        <section className="contact-section contact-name">
+          <label className="contact-label_name" htmlFor="name-input">
+            Name:
+          </label>
+          <input
+            id="name-input"
+            name="name"
+            type="text"
+            value={formData.name}
+            onChange={handleInputChange}
+          />
+        </section>
+        <section className="contact-section contact-email">
+          <label className="contact-label_email" htmlFor="email-input">
+            Email:
+          </label>
+          <input
+            id="email-input"
+            name="email"
+            type="email"
+            value={formData.email}
+            onChange={handleInputChange}
+          />
+        </section>
+        <section className="contact-section contact-message">
+          <label className="contact-label_message" htmlFor="message-input">
+            Message:
+          </label>
+          <textarea
+            id="message-input"
+            name="message"
+            value={formData.message}
+            onChange={handleInputChange}
+          />
+        </section>
+        <button className="contact-btn" type="submit">
+          Send
+        </button>
+      </form>
     </div>
-  )
-}
+  );
+};
+
+export default Contact;
